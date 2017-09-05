2e3b14ea4e650e73e3074299b396b510248d2629
Eldhose John <eldhosejohn1999@gmail.com>
2017-09-05 14:46:51 +0530

    initial commit

---
+++ src/main/java/com/devhopeslabs/zmemos/zMemos.java
@@ -0,0 +1,11 @@
+package com.devhopeslabs.zmemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class zMemos {//main app
+    public static void main(String[] args) {
+        SpringApplication.run(zMemos.class,args);
+    }
+}
